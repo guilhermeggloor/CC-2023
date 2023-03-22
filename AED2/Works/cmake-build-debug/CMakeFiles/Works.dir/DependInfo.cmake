475825ef6537b75b29c6105a02a9ab765da81ec4
guilhermeggloor <guilhermegloor8@gmail.com>
2023-03-21 21:34:40 -0400

    added cmake-build-debug from clion

---
+++ AED2/Works/cmake-build-debug/CMakeFiles/Works.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ggloor/Documents/CC/AED2/Works/W1/circle_square.c" "CMakeFiles/Works.dir/W1/circle_square.c.o" "gcc" "CMakeFiles/Works.dir/W1/circle_square.c.o.d"
+  "/home/ggloor/Documents/CC/AED2/Works/W1/gfx/gfx.c" "CMakeFiles/Works.dir/W1/gfx/gfx.c.o" "gcc" "CMakeFiles/Works.dir/W1/gfx/gfx.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AED2/Works/cmake-build-debug/CMakeFiles/Works.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Works.dir/W1/circle_square.c.o"
+  "CMakeFiles/Works.dir/W1/circle_square.c.o.d"
+  "CMakeFiles/Works.dir/W1/gfx/gfx.c.o"
+  "CMakeFiles/Works.dir/W1/gfx/gfx.c.o.d"
+  "Works"
+  "Works.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Works.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
