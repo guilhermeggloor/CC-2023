09223b1d9c6f5f3b330cd2dec4f1d20a1036f731
guilhermeggloor <guilhermegloor8@gmail.com>
2023-03-23 15:23:18 -0400

    changes of the destructor code

--- PP/Classes_Objetos/Destrutor/main.cpp
+++ PP/Classes_Objetos/Destrutor/main.cpp
@@ -1,60 +1,41 @@
 #include <iostream>
-#include <string.h>
-// #define NULL nullptr
+#include <cstring>
 
 using namespace std;
 
 class DinamicIntArray {
+private:
     int *ptr, size;
 
 public:
+    DinamicIntArray():size(16) { ptr = new int[size]; }
 
-    
-    DinamicIntArray():size(16)
-    { ptr = new int[size];}
+    explicit DinamicIntArray(int size):size(size) { ptr = new int[size]; }
 
-    explicit DinamicIntArray(int size):size(size)
-    { ptr = new int[size];}
-
-    DinamicIntArray(const DinamicIntArray &other){
-        if (ptr != nullptr)
-            delete[] ptr;        
-
+    DinamicIntArray(const DinamicIntArray &other) {
         size = other.size;
         ptr = new int[size];
         memcpy(ptr, other.ptr, sizeof(int)*size);
     }
 
-    // ~DinamicIntArray(){
-    //     delete []ptr;
-    // }
+    ~DinamicIntArray() { delete [] ptr; }
 
-//    DinamicIntArray(const int *&p, int size):size(size), ptr(new int[size])
-//    { memcpy(ptr, p, sizeof(int)*size) ;}
+    int getI(int index) const { return ptr[index]; }
 
-    int getI(int index) const {
-        return ptr[index];
-    }
+    int getSize() const { return size; }
 
-    void setI(int index, int value) {
-        ptr[index] = value;
-    }
-
-    int getSize() const {
-        return size;
-    }
+    void setI(int index, int value) { ptr[index] = value; }
 };
 
-int main(){
-    DinamicIntArray v1(5), v2;
+int main() {
+    DinamicIntArray v1(5);
 
-    for(int i=0; i<5; ++i) {
-        v1.setI(i, i+1);
-    }
+    for (int i = 0; i < 5; ++i) { v1.setI(i, i + 1); }
 
-    v2 = v1;
+    DinamicIntArray v2(v1);
 
-    cout << "Index: " <<  v2.getI(3) << " Size: " << v2.getSize();
+    v1.setI(3,0);
+    cout << "Index:" << v2.getI(3) << " Size:" << v2.getSize() << endl;
 
     return 0;
 }
