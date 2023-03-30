2496c480b7f112f055eb2974287715313c131bec
guilhermeggloor <guilhermegloor8@gmail.com>
2023-03-30 09:34:39 -0400

    adicionando trabalho

--- AED2/Works/W1/circle_square.c
+++ AED2/Works/W1/circle_square.c
@@ -1,11 +1,42 @@
 #include "gfx.h"
 #include <stdio.h>  /* printf */
 #include <unistd.h> /* sleep  */
+#include <math.h>
+
+// void Square(float base, float height)
+// {
+
+// }
+
+// void Circle(int x, int y, int rx, int ry)
+// {
+
+
+// }
+
+void Escolha()
+{
+	int input;
+	printf("\nEscolha entre 1 ou 2 entre as opções abaixo: \n");
+	printf("\n 1 - Circulo ");
+	printf("\n 2 - Quadrado ");
+	printf("\nDigite a opção escolhida: ");
+	scanf("%d", &input);
+
+	// if(input != 1 && 2)
+	// {
+		
+
+	// }
+
+}
 
 int main()
 {
 
 	gfx_init(600, 600, "W1 AED2");
+
+	Escolha();
 
 	gfx_paint();
 
